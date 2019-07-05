70f3d107524331a54ed6f7446b75200217a2fac4
wuwf <272551766@qq.com>
2019-07-05 18:17:07 +0800

    1

--- src/main/java/com/example/sentinelzuul/filter/UrlFilter.java
+++ src/main/java/com/example/sentinelzuul/filter/UrlFilter.java
@@ -1,5 +1,8 @@
 package com.example.sentinelzuul.filter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
@@ -9,6 +12,7 @@ import java.io.IOException;
  * @author wuweifeng wrote on 2018/11/1.
  */
 public class UrlFilter implements Filter {
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
 
     @Override
@@ -18,12 +22,12 @@ public class UrlFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
-
+        //
         String path = httpRequest.getRequestURI();
-        if (path.contains("/sentinelzuul")) {
-            path = path.replace("/sentinelzuul", "");
-            httpRequest.getRequestDispatcher(path).forward(httpRequest, response);
-        }
-
+        //if (path.contains("/sentinelzuul")) {
+        //    path = path.replace("/sentinelzuul", "");
+        //    httpRequest.getRequestDispatcher(path).forward(httpRequest, response);
+        //}
+        logger.info("path:" + path);
     }
 }
